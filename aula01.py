b07d9eadd77c2155aca2a47a441051e57150a9a9
Capeladro <pedro.fracaro.martins@escola.pr.gov.br>
2024-03-27 10:39:07 -0300

    subir aula01

---
+++ aula01.py
@@ -0,0 +1,10 @@
+import os
+
+#print("Olá mundo cruel")
+print("O 3DS é a turma numero 1 do Sagrada")
+turma="3DS"
+pos=1
+print("O",turma,"é a turma número", pos, "do Sagrada")
+print(type(turma))
+print(type(pos))
+os.system("clear")
